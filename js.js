077122cc31f4f0ebee40bcccfca17e2bae2df47a
crecheng <1935598195@qq.com>
2021-01-29 13:31:33 +0800

    add count time

--- js.js
+++ js.js
@@ -11,14 +11,43 @@ var myDiagram =
 
 // the template we defined earlier
 myDiagram.nodeTemplate =
-  $(go.Node, "Horizontal",
+  $(
+    go.Node, "Horizontal",
     { background: "#44CCFF" },
-    //$(go.Picture,
-    //  { margin: 10, width: 50, height: 50, background: "red" },
-    //  new go.Binding("source")),
+  //$(go.Picture,
+  //  { margin: 10, width: 50, height: 50, background: "red" },
+  //  new go.Binding("source")),
+  $(go.Panel, "Table",
+    
+    $(go.RowColumnDefinition, { column: 1, width: 3 }),
     $(go.TextBlock, "Default Text",
-      { margin: 12, stroke: "#000000", font: "bold 16px sans-serif" },
-      new go.Binding("text", "name"))
+      {
+        row:0,
+        column:0,
+        margin: new go.Margin(8,8,0,8),
+        stroke: "#000000",
+        font: "bold 16px sans-serif" 
+      },
+      new go.Binding("text", "name")),
+      $(go.TextBlock, "数量",
+      {
+        row:1,
+        column:0,
+        margin: new go.Margin(0,8,0,8), 
+        stroke: "#555555", 
+        font: "12px sans-serif" 
+      },
+      new go.Binding("text", "count")),
+      $(go.TextBlock, "数量",
+      {
+        row:2,
+        column:0,
+        margin: new go.Margin(0,8,8,8), 
+        stroke: "#555555", 
+        font: "12px sans-serif" 
+      },
+      new go.Binding("text", "me")),
+      ),
   );
 
 // define a Link template that routes orthogonally, with no arrowhead
@@ -31,7 +60,7 @@ myDiagram.linkTemplate =
 var model = $(go.TreeModel);
 
 var recipes=[];
-function d(id){
+function d(id,count){
     recipes=[];
     var temp={
         key:"",
@@ -40,15 +69,27 @@ function d(id){
     temp.key=id+"|"+0;
     temp.name=data[id].name;
     temp.source="";
-
+    if(data[id].make===undefined){
+      temp.me="";
+      temp.count="";
+    }else{
+      var make=data[id].make
+      if(count===undefined){
+        count=60/make.time;
+      }
+      temp.count=count+"/min";
+      temp.me=1.0*count/make.count*make.time/60.0;
+      temp.me=myFix(temp.me)
+      
+    }
     recipes.push(temp);
-    doMake(id,0,temp.key);
+    doMake(id,temp.key,count);
     model.nodeDataArray=recipes;
     myDiagram.model=model;
     
 }
 
-function doMake(id,c,parent){
+function doMake(id,parent,count){
     if(data[id].level>0){
         data[id].make.item.forEach(e => {
             var temp={
@@ -58,16 +99,35 @@ function doMake(id,c,parent){
             if(data[e.id]===undefined){
                 console.log(e.id);
             }
-            temp.key=e.id+"|"+(c+1)+"-"+Math.random();
+            var needcount=count/data[id].make.count;
+            var make=data[id].make;
+            temp.key=e.id+"|"+Math.floor(Math.random()*100);
             temp.name=data[e.id].name;
             temp.parent=parent;
             temp.source="img/1.jpg";
+            temp.count=needcount*e.count+"/min";
+            if(data[e.id].level>0){
+              temp.me=1.0*needcount*e.count/make.count*make.time/60.0;
+              temp.me=myFix(temp.me)
+            }
+            else{
+              temp.me=""
+            }
             recipes.push(temp);
-            doMake(e.id,c+1,temp.key);
+            doMake(e.id,temp.key,needcount*e.count);
         });
     }
 }
 
+
+function myFix(str){
+  str=String(str);
+  var index= str.indexOf(".");
+  if(index<1)
+    return str;
+  else
+    return str.slice(0, index + 3);
+}
 
 d("tc");
 
